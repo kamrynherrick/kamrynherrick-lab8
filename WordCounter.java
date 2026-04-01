f49d541072aa99d993bc4aadf8827502188048a8
Kamryn Herrick <kamryn.herrick@gwu.edu>
2026-04-01 01:21:08 -0400

    updated wordcounter

--- WordCounter.java
+++ WordCounter.java
@@ -7,6 +7,46 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class WordCounter {
-    public static int processText()
+
+    //counts words in text up to the stopword, or all text if the stopword is null
+    //throws tooSmallText if total word count is less than 5
+    //throws invalidStopwordException if stopword is not found in the text
+    public static int processText(StringBuffer text, String stopword) {
+
+        //define word as a sequence of letters, digits, or quotes
+        Pattern regex = Pattern.compile("[a-zA-Z0-9]+"); 
+        Matcher m = regex.matcher(text); 
+
+        //first pass: count every word in the text
+        int total = 0; 
+        while (m.find()) {
+            total++; 
+        }
+
+        //fewer than 5 words will cause text too small to be called
+        if (total < 5) throw new TooSmallText(total); 
+
+        //no stopword means to count all words, total will return what we already have as wordcount
+        if (stopword == null) {
+            return total; 
+        }
+
+        //second pass: count words until hit the stopword
+        m.reset(); 
+        int count = 0; 
+        while(m.find()) {
+            count++; 
+
+            if (m.group().equals(stopword)) {
+                return count; 
+            }
+        }
+
+        //if the loop finishes without stopword, raise exception
+        throw new InvalidStopWordException(stopword); 
+
+    }
+
+    
     
 }
